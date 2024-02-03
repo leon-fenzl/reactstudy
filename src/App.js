a729663eaec024d86cc59a6e8c228a6226e418e5
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-02-02 21:53:16 -0300

    update 00

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import "./Components/Styles/main.css";
+import Navbar from './Components/Navbar/Navbar.js';
+import Homepage from "./Components/Body/Homepage.js";
+import Game from "./Components/Body/Game.js"
+import About from "./Components/Body/About.js";
+import Footer from './Components/Footer/Footer.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return(
+    <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/game" element={<Game/>}/>
+        </Routes>
+      <Footer/>
+    </BrowserRouter>
+  )
 }
-
-export default App;
---
+++ src/Components/Body/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import "../Styles/body.css";
+export default function About(){
+  return(
+    <section className="title">
+      <h1>About</h1>
+    </section>
+  );
+}
